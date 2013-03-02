a52d4f472041a4d559b43ecb65ae99f0d330a885
Alastair McClelland <alastair.mcclelland@gmail.com>
2013-03-02 12:47:44 +0000

    Finished problem 24

--- src/common/Problem24.cpp
+++ src/common/Problem24.cpp
@@ -10,3 +10,75 @@ Problem24::~Problem24()
 {
 
 }
+
+string Problem24::answer()
+{
+  string answer = "";
+  vector<string> stringPerms;
+  this->getPermutations(&stringPerms, "0123456789");
+  answer = stringPerms.at(999999);
+  return answer;
+}
+
+void Problem24::getPermutations(vector<string>* permutations,
+                                string parts)
+{
+  if(parts.size() == 2)
+  {
+    string first = parts.substr(0,1);
+    string second = parts.substr(1,1);
+
+    permutations->push_back(first + second);
+    permutations->push_back(second + first);
+  }
+  else
+  {
+    for(unsigned j = 0; j < parts.size(); ++j)
+    {
+      string first = parts.substr(j,1);
+      string rest = this->getStringWithoutIndex(parts, j);
+
+      vector<string> nextSet;
+      this->getPermutations(&nextSet, rest);
+
+      for(unsigned i = 0; i < nextSet.size(); ++i)
+      {
+        string perm = first + nextSet.at(i);
+        permutations->push_back(perm);
+      }
+    }
+  }
+}
+
+string Problem24::combineVectorString(vector<string> parts)
+{
+  string returnValue;
+  for(unsigned i = 0; i < parts.size(); ++i)
+  {
+    returnValue += parts.at(i);
+  }
+  return returnValue;
+}
+
+string Problem24::getStringWithoutIndex(string input, unsigned index)
+{
+  string returnValue;
+  for(unsigned i = 0; i < input.size(); ++i)
+  {
+    if(i != index)
+    {
+      returnValue += input.substr(i,1);
+    }
+  }
+  return returnValue;
+}
+
+vector<int> Problem24::convertStringVectorToIntVector(vector<string> input)
+{
+  vector<int> output;
+  for(unsigned i = 0; i < input.size(); ++i)
+  {
+    output.push_back(atoi(input.at(i).c_str()));
+  }
+  return output;
+}
--- src/test/common/Problem24Test.h
+++ src/test/common/Problem24Test.h
@@ -2,8 +2,12 @@
 #define PROBLEM_24_TEST_H
 
 #include <cppunit/extensions/HelperMacros.h>
+#include <vector>
+#include <string>
 
 #include "Problem24.h"
+
+using namespace std;
 
 class Problem24Test : public CppUnit::TestFixture
 {
@@ -11,12 +15,22 @@ class Problem24Test : public CppUnit::TestFixture
 
     void setUp();
 
+    void testGetPermutationsOf0And1();
+    void testCombineVector();
+    void testGetStringWithoutChar();
+    void testConvertStringVectorToIntVector();
+    void testAnswer();
     //NEXT_TEST_DEC
 
     void tearDown();
 
     CPPUNIT_TEST_SUITE( Problem24Test );
 
+    CPPUNIT_TEST( testGetPermutationsOf0And1 );
+    CPPUNIT_TEST( testCombineVector );
+    CPPUNIT_TEST( testGetStringWithoutChar );
+    CPPUNIT_TEST( testConvertStringVectorToIntVector );
+    CPPUNIT_TEST( testAnswer );
     //NEXT_TEST_ADD
 
     CPPUNIT_TEST_SUITE_END();
